9cef68ac6dda87401cad2543dbd41fd539eff937
Myronarty <ahamalianmyron@gmail.com>
2026-03-10 02:52:24 +0200

    +gcd

--- NTA_lab1/Func_names.h
+++ NTA_lab1/Func_names.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+#include <random>
+#include <cstdint>
 
 using namespace std;
 
 pair<uint64_t, uint64_t> lob(uint64_t p);
 
-uint64_t SSh(uint64_t p);
+uint64_t gcd(uint64_t x, uint64_t p);
 
-uint64_t Pol(uint64_t p);
+pair<uint64_t, uint64_t> SSh(uint64_t p, int tochnist);
 
-uint64_t B_M(uint64_t p);
+pair<uint64_t, uint64_t> Pol(uint64_t p);
+
+pair<uint64_t, uint64_t> B_M(uint64_t p);
--- NTA_lab1/Funcs.cpp
+++ NTA_lab1/Funcs.cpp
@@ -16,20 +16,38 @@ pair<uint64_t, uint64_t> lob(uint64_t p)
 	return pair(1, p);
 }
 
-uint64_t SSh(uint64_t p)
+uint64_t gcd(uint64_t x, uint64_t p)
 {
-
-	return 1;
+	while (x != 0) 
+	{
+		p %= x;
+		swap(x, p);
+	}
+	cout << p;
+	return p;
 }
 
-uint64_t Pol(uint64_t p)
+pair<uint64_t, uint64_t> SSh(uint64_t p, int tochnist)
 {
+	for (int i = 0; i < tochnist; i++)
+	{
+		mt19937_64 gen(random_device{}());
+		uniform_int_distribution<uint64_t> distribution(0, numeric_limits<uint64_t>::max());
+		uint64_t x = distribution(gen);
 
-	return 1;
+
+	}
+	return pair(1, p);
 }
 
-uint64_t B_M(uint64_t p)
+pair<uint64_t, uint64_t> Pol(uint64_t p)
 {
 
-	return 1;
+	return pair(1, p);
+}
+
+pair<uint64_t, uint64_t> B_M(uint64_t p)
+{
+
+	return pair(1, p);
 }
